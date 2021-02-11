87b43fe80202fe3f2bde328a814240e8a3026bad
mirandait-services <78923269+mirandait-services@users.noreply.github.com>
2021-02-11 16:05:46 -0300

    Create install.sh

---
+++ docker-compose/Script/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Download dos scripts
+git clone https://github.com/mirandait-services/monitoramento
+cd monitoramento
+
+#Instalação do Docker
+apt update -y
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update -y
+apt install docker-ce -y
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+#
+
+#Docker-compose
+docker-compose -f docker-compose/zabbix/docker-compose.yml -f docker-compose/graylog/docker-compose.yml up
+#
